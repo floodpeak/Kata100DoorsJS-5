768f56748bb202dcfe9d6b1637175529eb292fde
floodpeak <hongfengzhou@yahoo.com>
2016-10-23 23:58:09 +0800

    rf, combine the logic of push 2 for door 4 and 6

--- getDoorTogglePoints.js
+++ getDoorTogglePoints.js
@@ -1,11 +1,10 @@
 export default (door)=>{
   const result = [1]
-  if(door === 4){
+  if(door % 2 === 0 && door !== 2){
     result.push(2)
   }
   if(door === 6){
-    result.push(2)
-    result.push(3) 
+    result.push(3)
   }
   if(door > 1){
     result.push(door)
